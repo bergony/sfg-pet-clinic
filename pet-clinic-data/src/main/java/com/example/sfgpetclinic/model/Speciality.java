11a121faa70b2ccaaa83eea2ffdb3d8092f3a806
Your Name <you@example.com>
2021-02-20 23:01:14 -0300

    Added Speciality, update Vet with Speciality relationship. Closes #27

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,12 @@
+package com.example.sfgpetclinic.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+}
--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
@@ -1,4 +1,10 @@
 package com.example.sfgpetclinic.model;
 
+import java.util.Set;
+
+@Ge
 public class Vet extends Person {
+
+    private Set<Speciality> specialities;
+
 }
