030962bb2c14bd3d9cd8023e6d9952d146ca984e
Your Name <you@example.com>
2021-02-22 21:30:32 -0300

    update base entity. Closes #35.

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/BaseEntity.java
@@ -3,12 +3,19 @@ package com.example.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
 }
