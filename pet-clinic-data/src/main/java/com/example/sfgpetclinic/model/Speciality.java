cb8ea8b860cbe9b7ba05e3431cbaf5355c3e1531
Your Name <you@example.com>
2021-02-22 21:49:28 -0300

    Update  vets to use JPA entity Mappings. Closes #37.

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Speciality.java
@@ -3,10 +3,18 @@ package com.example.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
 }
--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Vet.java
@@ -3,13 +3,19 @@ package com.example.sfgpetclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
+@Entity
+@Table(name = "vets")
 public class Vet extends Person {
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"),
+            inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
 }
