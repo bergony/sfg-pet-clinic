c9a2405d3f74e9b76554f8331a1e3d6b757fb1b4
Your Name <you@example.com>
2021-02-23 12:51:24 -0300

    Implemented Map impl for Visit serivce. Closes #46.

---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.example.sfgpetclinic.services;
+
+import com.example.sfgpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package com.example.sfgpetclinic.services.map;
+
+import com.example.sfgpetclinic.model.Visit;
+import com.example.sfgpetclinic.services.VisitService;
+
+import java.util.Set;
+
+public class VisitMapService extends AbstracMapService<Visit, Long> implements VisitService {
+
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+
+        if(visit.getPet() == null ||
+                visit.getPet().getOwner() == null ||
+                visit.getPet().getId() == null ||
+                visit.getPet().getOwner().getPets() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return super.findByID(aLong);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        super.deleteByid(aLong);
+    }
+}
