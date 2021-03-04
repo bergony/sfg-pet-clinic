a0e5e9722a2775c9dd2f23243fec97036ad09997
Your Name <you@example.com>
2021-03-04 16:09:46 -0300

    Added json endpoint. closes #71

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.example.sfgpetclinic.controllers;
 
+import com.example.sfgpetclinic.model.Vet;
 import com.example.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -16,8 +21,12 @@ public class VetController {
 
     @RequestMapping({"/vets", "/vets/index/", "/vets/index.html", "/vets.html"})
     public String listVets(Model model){
-
         model.addAttribute("vets", vetService.findAll());
         return  "vets/index";
+    }
+
+    @GetMapping("/api/vets/")
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
